a5afb69dd12b1efee2e786aa27a5135143db1026
SpeedOfLight03 <arnab.ghosh.it2021@kgec.ac.in>
2023-11-14 10:34:38 +0530

    modified code

--- AI Desktop Assistant/code.py
+++ AI Desktop Assistant/code.py
@@ -1,1 +1,13 @@
-print("Hello")
+import speech_recognition as sr
+import wikipedia
+import openai
+import pyaudio
+import os
+
+
+def say(text):
+    os.system(f"Say {text}")
+
+if __name__ == "__main__":
+    print("Hello")
+    say("Hello I am Jarvis A.I")
